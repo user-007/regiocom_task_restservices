6242755f5d0ff21aad326c1501484b52b0d724d3
Yoan Ivanov <yoan.ivanov0007@gmail.com>
2023-08-02 08:18:40 +0300

    initial commit

---
+++ src/main/java/com/regiocom/task_regiocom/Classes/Admin.java
@@ -0,0 +1,13 @@
+package com.regiocom.task_regiocom.Classes;
+
+import javax.management.relation.Role;
+//see all contents and EDIT and DELETE all contents
+
+public class Admin {
+    private int admin;
+    public void role(String role,String roleText){
+        //to be completed....
+
+    }
+
+}
